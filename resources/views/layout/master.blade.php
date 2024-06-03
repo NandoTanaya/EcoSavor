974ea917e4a8bc3db5897b5f9cd41f783ab2f462
Made Nando <i.mahatresna@binus.ac.id>
2024-06-03 15:56:40 +0700

    Menambahkan dan mengganti about, menghubungkan about - register -login page

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,12 +1,17 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
+  <!--divinectorweb.com-->
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+
+    {{-- CSS dari login dan register --}}
     <title>@yield('title', 'Custom Laravel')</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     <link href="{{ asset('css/register.css') }}" rel="stylesheet">
+
     <style>
         :root {
             --primary: #664c25;
@@ -41,43 +46,85 @@
             margin-left: 0 !important;
         }
     </style>
+
+    {{-- CSS register dan login END --}}
+
+
+    <!-- All CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
+    />
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Fredoka -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap"
+      rel="stylesheet"
+    />
+
+    {{-- ICONS --}}
+    <script src="https://unpkg.com/feather-icons"></script>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light w-100">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{config('app.name')}}">EcoSavor</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav mx-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Buy</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Donate</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Cart</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Login</a>
-                    </li>
-                </ul>
-            </div>
+    {{-- Navbar start --}}
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="about">EcoSavor</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link" href="home">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="about">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#partner">Partner</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#donate">Donate</a>
+                </li>
+                {{-- <li class="nav-item">
+                  <a class="nav-link" href="#buy">Buy</a>
+                </li> --}}
+                {{-- <li class="nav-item">
+                  <a class="nav-link" href="#sell">Sell</a>
+                </li> --}}
+              <li class="nav-item">
+                <a class="nav-link" href="register"><i data-feather="log-in"></i></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="profile"><i data-feather="user"></i></a>
+              </li>
+            </ul>
+          </div>
         </div>
-    </nav>
+      </nav>
+    {{-- Navbar END --}}
 
+    {{-- ICONS --}}
+    <script>
+        feather.replace();
+    </script>
+    <!-- All Js -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     @yield('content')
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   </body>
-  
 </html>
-
