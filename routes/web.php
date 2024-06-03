ca85bbfe0add27a60d17ee37e003ef4813e958f4
Yorokoube <iwataninaofumi0909@gmail.com>
2024-06-03 18:17:59 +0700

    blok

--- routes/web.php
+++ routes/web.php
@@ -4,36 +4,12 @@ use App\Http\Controllers\AuthManager;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('about');
+    return view('welcome');
 });
 
 Route::get('/about', function () {
     return view('about');
-});
-
-Route::get('/partner', function() {
-    return view('partner');
-});
-
-Route::get('/unggah', function() {
-    return view('unggah');
-});
-
-Route::get('/menu', function() {
-    return view('menu');
-});
-
-Route::get('/status', function() {
-    return view('status');
-});
-
-Route::get('/laporan', function() {
-    return view('laporan');
-});
-
-Route::get('/profilePartner', function() {
-    return view('profilePartner');
-});
+})->name('home');
 
 Route::get('/login', [AuthManager::class, 'login'])->name('login');
 Route::post('/login', [AuthManager::class, 'loginPost'])->name('login.post');
