852caf1b095a7b6e60fdb6547133a43728fb520b
rahelloo <rahelarabella@gmail.com>
2024-06-17 14:52:47 +0700

    "Integrasi template admin"

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,18 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>ADMIN</h1>
+<html>
+  <head>
+    @include('admin.css')
+  </head>
+  <body>
+    <!-- Navbar -->
+    @include('admin.navbar')
 
-    <!-- Button logout dari tampilan, bukan dari kehidupan-->
-    <form method="POST" action="{{ route('logout')}}">
-        @csrf
-        <input type="submit" value="Logout">
-    </form>
-</body>
+
+    <!-- Sidebar start -->
+    @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+      <div class="page-content">
+        <div class="page-header">
+          <div class="container-fluid">
+            <!-- Body til footer here -->
+            @include('admin.body')
+
+          </div>
+      </div>
+    </div>
+    <!-- JavaScript files-->
+    <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
+    <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
+    <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('admincss/js/charts-home.js')}}"></script>
+    <script src="{{asset('admincss/js/front.js')}}"></script>
+  </body>
 </html>
